7965589602a979204f1c4618457504feaa4462d8
B.D.Heshan Madhuranga IT20611910 <39455525+heshanm27@users.noreply.github.com>
2022-07-05 15:20:35 +0530

    app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/index";
+
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "STALKER1",
+    },
+  });
   return (
-    <Routes>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import BottomNavBar from "../../components/BottomNavBar/BottomNavBar";
 
 export default function index() {
   return (
-    <div>
-      <h1>index hello</h1>
+    <div style={{ backgroundColor: "#070B2E", height: "100vh" }}>
+      <Container maxWidth="xl" sx={{ pt: 10 }}>
+        <Box sx={{ p: 2 }}>
+          <Stack direction="row" alignItems="center" justifyContent={"center"}>
+            <Stack direction="column" alignItems="center">
+              <Typography variant="h5" alignItems="center" color="white">
+                Hello I'm
+              </Typography>
+              <Typography variant="h2" alignItems="center" color="white">
+                Heshan Madhuranga
+              </Typography>
+              <Typography variant="h5" alignItems="center" color="white">
+                Fullstack Developer
+              </Typography>
+            </Stack>
+          </Stack>
+        </Box>
+        <Stack direction="row" alignItems="center" justifyContent={"center"}>
+          <BottomNavBar />
+        </Stack>
+      </Container>
     </div>
   );
 }
